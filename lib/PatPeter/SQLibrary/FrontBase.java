750085c548de545ebb3e58d3db34328eed5ebbeb
unknown <Solin@ANEMONE.(none)>
2013-02-25 22:59:05 -0600

    Commit 4.2 (release).

--- lib/PatPeter/SQLibrary/FrontBase.java
+++ lib/PatPeter/SQLibrary/FrontBase.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import java.util.logging.Logger;
 
 import lib.PatPeter.SQLibrary.Delegates.HostnameDatabase;
-import lib.PatPeter.SQLibrary.Delegates.HostnameDatabaseImpl;
+import lib.PatPeter.SQLibrary.Factory.DatabaseFactory;
 
 /**
  * Child class for the FrontBase database.<br>
@@ -14,7 +14,7 @@ import lib.PatPeter.SQLibrary.Delegates.HostnameDatabaseImpl;
  * @author Nicholas Solin, a.k.a. PatPeter
  */
 public class FrontBase extends Database {
-	private HostnameDatabase delegate = new HostnameDatabaseImpl();
+	private HostnameDatabase delegate = DatabaseFactory.hostname();
 	
 	public enum Statements implements StatementEnum {}
 	
