b34becab9e887b7cc294705bf2b1d5b8cd2f196c
Nicholas Solin <solin.nick@gmail.com>
2013-10-13 14:23:50 -0500

    Correct and move files.

--- src/main/java/lib/PatPeter/SQLibrary/SQLibrarySpout.java
+++ src/main/java/lib/PatPeter/SQLibrary/SQLibrarySpout.java
@@ -1,16 +1,19 @@
 package lib.PatPeter.SQLibrary;
 
-import org.spout.api.plugin.Plugin;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class SQLibrarySpout extends Plugin {
+//import org.spout.api.plugin.Plugin;
+
+public class SQLibrarySpout /*extends Plugin*/ {
 	public static final Logger logger = Logger.getLogger("Minecraft");
 	
-	@Override 
+	//@Override 
 	public void onEnable() {
 		SQLibrary.logger.log(Level.INFO, "SQLibrary loaded.");
 	}
 	
-	@Override
+	//@Override
 	public void onDisable() {
 		SQLibrary.logger.log(Level.INFO, "SQLibrary stopped.");
 	}
