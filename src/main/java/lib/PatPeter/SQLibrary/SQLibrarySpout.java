5ec5a287698e9e1c51f75d40985bfc29a3c15417
Nicholas Solin <solin.nick@gmail.com>
2013-10-14 23:18:20 -0500

    Fix build error.

--- src/main/java/lib/PatPeter/SQLibrary/SQLibrarySpout.java
+++ src/main/java/lib/PatPeter/SQLibrary/SQLibrarySpout.java
@@ -3,17 +3,17 @@ package lib.PatPeter.SQLibrary;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-//import org.spout.api.plugin.Plugin;
+import org.spout.api.plugin.Plugin;
 
-public class SQLibrarySpout /*extends Plugin*/ {
+public class SQLibrarySpout extends Plugin {
 	public static final Logger logger = Logger.getLogger("Minecraft");
 	
-	//@Override 
+	@Override 
 	public void onEnable() {
 		SQLibrary.logger.log(Level.INFO, "SQLibrary loaded.");
 	}
 	
-	//@Override
+	@Override
 	public void onDisable() {
 		SQLibrary.logger.log(Level.INFO, "SQLibrary stopped.");
 	}
